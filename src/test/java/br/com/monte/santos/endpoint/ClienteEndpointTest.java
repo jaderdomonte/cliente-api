8d88129bf1e9276738581aa16b49caf76e1ceead
GPI - Jader Augusto do Monte Santos <P51701051@brasilprev.com.br>
2019-04-18 17:59:45 -0300

    Increment Unit Tests

--- src/test/java/br/com/monte/santos/endpoint/ClienteEndpointTest.java
+++ src/test/java/br/com/monte/santos/endpoint/ClienteEndpointTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -43,6 +44,40 @@ public class ClienteEndpointTest {
 		
 		assertThat(response.getStatusCodeValue()).isEqualTo(200);
 	}
+	
+	@Test
+	public void deveRetornarCodigo200AoConsultarClientePorId() {
+		Cliente cliente = criarCliente(1L);
+		BDDMockito.when(service.consultarPorId(cliente.getId())).thenReturn(cliente);
+		
+		ResponseEntity<String> response = restTemplate.getForEntity("/v1/clientes/{id}", String.class, cliente.getId());
+		
+		assertThat(response.getStatusCodeValue()).isEqualTo(200);
+	}
+	
+	@Test
+	public void deveRetornarCodigo404AoConsultarClientePorIdInexistente() {
+		Long idCliente = 3L;
+		BDDMockito.when(service.consultarPorId(idCliente)).thenReturn(null);
+		
+		ResponseEntity<String> response = restTemplate.getForEntity("/v1/clientes/{id}", String.class, idCliente);
+		
+		assertThat(response.getStatusCodeValue()).isEqualTo(404);
+	}
+	
+	@Test
+	public void deveRetornarCodigo201AoSalvarCliente() {
+		Cliente cliente = criarCliente(1L);
+		BDDMockito.when(service.salvar(cliente)).thenReturn(cliente);
+		
+		ResponseEntity<String> response = restTemplate.postForEntity("/v1/clientes/", cliente, String.class); 
+		
+		assertThat(response.getStatusCodeValue()).isEqualTo(201);
+	}
+
+	private Cliente criarCliente(Long id) {
+		return Cliente.builder().id(id).nome("Cliente "+id).build();
+	} 
 
 	private List<Cliente> criarListaDeClientes() {
 		return Arrays.asList(Cliente.builder().id(1L).nome("Cliente 1").build(), Cliente.builder().id(2L).nome("Cliente 2").build());
