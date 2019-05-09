f5bea34ac2ecd4ee78f260d9b36d6b689a048e97
Jader Augusto do Monte Santos <jaderdomonte@gmail.com>
2019-05-09 00:24:38 -0300

    Ajuste build

--- src/main/java/br/com/monte/santos/model/Cliente.java
+++ src/main/java/br/com/monte/santos/model/Cliente.java
@@ -3,12 +3,10 @@ package br.com.monte.santos.model;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-
-import org.hibernate.annotations.GenericGenerator;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,8 +24,7 @@ import lombok.ToString;
 public class Cliente {
 	
 	@Id
-	@GeneratedValue(generator = "increment")
-	@GenericGenerator(name = "increment", strategy = "increment")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	
 	private String nome;
--- src/main/java/br/com/monte/santos/model/Localizacao.java
+++ src/main/java/br/com/monte/santos/model/Localizacao.java
@@ -4,10 +4,8 @@ import java.math.BigDecimal;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.hibernate.annotations.GenericGenerator;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,8 +23,7 @@ import lombok.ToString;
 public class Localizacao {
 	
 	@Id
-	@GeneratedValue(generator = "increment")
-	@GenericGenerator(name = "increment", strategy = "increment")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	
 	private String idLocalizacaoMundial;
